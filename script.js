7c443917aadd7d4dd43a7a9cfd19cdfc26b322c9
tejasveedighe <tejasveedighe@gmail.com>
2024-06-11 22:55:53 +0530

    new landing page with responsiveness

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function openNav() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
 }
+
+function onEbookFormSubmit(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	const name = data.get("user-email");
+}
+
+const form = document.getElementById("ebook-form");
+form.addEventListener("submit", onEbookFormSubmit);
